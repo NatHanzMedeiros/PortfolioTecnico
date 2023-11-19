2da12c743c41476012d03a8f011b6f0cbaa4673d
Nathan Medeiros Cristiano <nathan_cristiano@estudante.sesisenai.org.br>
2023-11-19 02:14:44 -0300

    Create  MacBookPro161.js

---
+++ 3º ano/React Landing Page | 3º Trimestre/MacBookPro161.js
@@ -0,0 +1,42 @@
+import styles from "./MacBookPro161.module.css";
+
+const MacBookPro161 = () => {
+  return (
+    <div className={styles.macbookPro161}>
+      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+      <img
+        className={styles.smartmockupsLn8ymt211Icon}
+        alt=""
+        src="/smartmockups-ln8ymt21-1@2x.png"
+      />
+      <div className={styles.petHubARede}>PET-HUB: A REDE DAS ONGS</div>
+      <img className={styles.usersIcon} alt="" src="/users.svg" />
+      <img className={styles.clipboardIcon} alt="" src="/clipboard.svg" />
+      <div className={styles.ongsEUsuariosContainer}>
+        <span className={styles.o}>o</span>
+        <span className={styles.ngsEUsuarios}>
+          ngs e usuarios poderam cumprir com metas e doações, aumentando seu
+          nivel e ganhando recompensas
+        </span>
+      </div>
+      <div className={styles.facilitandoAComunicaoContainer}>
+        <span className={styles.f}>F</span>
+        <span className={styles.ngsEUsuarios}>
+          ACILITANDO A COMUNICAÇÂO PARA ONGS E DOADORES, PARA FIM DE ADOÇÂO E
+          DOAÇÃO.
+        </span>
+      </div>
+      <div className={styles.nossoObjetivoEContainer}>
+        <span className={styles.f}>N</span>
+        <span className={styles.ngsEUsuarios}>
+          osso objetivo e amenizar a falta de visibilidade que as ongs tem.
+        </span>
+      </div>
+      <div className={styles.home} />
+      <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+      <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
+    </div>
+  );
+};
+
+export default MacBookPro161;
